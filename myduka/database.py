3f75bf68b302902bcf4ec0950139dc1b68483cfa
Daniel michael <michaellemakanit@gmail.com>
2026-06-05 14:40:22 +0300

    new commit

--- myduka/database.py
+++ myduka/database.py
@@ -37,100 +37,83 @@
 # # conn.close()
 
 import psycopg2
-
 #establishing a connection to a postgres db
-conn = psycopg2.connect(host='localhost',port=5432,user='postgres',password='2201',dbname='myduka')
-
-#cur object
-cur = conn.cursor()
-
+conn=psycopg2.connect(host='localhost',port=5432,user='postgres',password='9467',dbname='myduka')
+ #cursor object
+cur=conn.cursor()
 def get_products():
-    cur.execute("select * from products")
-    products_data = cur.fetchall()
+    cur.execute('select * from products')
+    products_data=cur.fetchall()
     return products_data
-    
+
+# cur.execute("insert into products(name,buying_price,selling_price)values('wallet',300,500)")
+# conn.commit()
+# print(products_data)
 
 def get_sales():
-    cur.execute("select * from sales")
-    sales_data = cur.fetchall()
+    cur.execute('select * from sales')
+    sales_data=cur.fetchall()
     return sales_data
 
-def get_stock():
-    cur.execute("select * from stock")
-    stock_data = cur.fetchall()
-    return stock_data
-
-
-def get_data(table):
-    cur.execute(f"select * from {table}")
-    data = cur.fetchall()
-    return data
-
-
-def insert_products(values):
-    cur.execute(f"insert into products(name,buying_price, selling_price)values{values}")
+def insert_products2(values):
+    cur.execute(f"insert into products(name,buying_price,selling_price)values{values}")
     conn.commit()
 
-product1 = ('shoes',2000,2500)
-product2 = ('phone',10000,15000)
-
-# insert_products(product1)
-# insert_products(product2)
-
+product1=('tissue',35,50)
+product2=('valon',120,150)
+# insert_products2(product1)
+# insert_products2(product2)
+products_data=insert_products2
+print(products_data)
 
 def insert_products2(values):
-    cur.execute("insert into products(name,buying_price,selling_price)values(%s,%s,%s)", values)
+    cur.execute(f"insert into products(name,buying_price,selling_price)values(%s,%s,%s)",values)
     conn.commit()
 
+product3=('charger',200,350)
+# insert_products2(product3)
+products_data=insert_products2
+print(products_data)
+# Q1
+def get_stocks():
+    cur.execute("select * from stock")
+    stocks_data = cur.fetchall()
+    return stocks_data
+
+stocks_data = get_stocks()
+print(stocks_data)
+
+#insert sales() 
 
 def insert_sales(values):
-    cur.execute("insert into sales(pid,quantity)values(%s,%s)", values)
+    cur.execute("insert into sales(pid,quantity)values(%s,%s)",values)
     conn.commit()
 
+sales1=(4,8)
+sales2=(3,12)
+sales3=(5,20)
 
+
+# insert_sales(sales1)
+# insert_sales(sales2)
+# insert_sales(sales3)
+
+
+sales_data=get_sales()
+print(sales_data)
+
+# -> insert_stock()
 def insert_stock(values):
-    cur.execute("insert into stock(pid,stock_quantity)values(%s,%s)", values)
+    cur.execute("insert into stock(pid,stock_quantity)values(%s,%s)",values)
     conn.commit()
 
+stock1=(2,16)
+stock2=(5,21)
+stock3=(7,35)
 
-product3 = ('laptop',40000,50000)
-insert_products2(product3)
+# insert_stock(stock1)
+# insert_stock(stock2)
+# insert_stock(stock3)
 
-
-def sales_per_day():
-    cur.execute("""
-      select date(sales.created_at) as date, sum(sales.quantity * products.selling_price) as
-      total_sales from sales join products on products.id = sales.pid  group by date;
-    """)
-    daily_sales = cur.fetchall()
-    return daily_sales
-
-
-def profit_per_day():
-    cur.execute("""
-        select date(sales.created_at) as date, sum(sales.quantity *( products.selling_price -
-        products.buying_price)) as total_sales from sales join products on products.id = sales.pid
-         group by date;
-    
-    """)
-    daily_profit = cur.fetchall()
-    return daily_profit
-
-
-
-def sales_per_product():
-    cur.execute("""
-        select products.name as p_name , sum(sales.quantity * products.selling_price)  as total_sales
-        from products join sales on sales.pid = products.id group by p_name;
-    """)
-    product_sales = cur.fetchall()
-    return product_sales
-
-
-def profit_per_product():
-    cur.execute("""
-        select products.name as p_name , sum(sales.quantity *( products.selling_price - 
-        products.buying_price))  as total_sales from products join sales on sales.pid = products.id group by p_name;
-    """)
-    product_profit = cur.fetchall()
-    return product_profit
+stocks_data = get_stocks()
+print(stocks_data)
