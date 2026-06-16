6f50142bd45feaa963b0a29f99c0717a23ad081e
Daniel michael <michaellemakanit@gmail.com>
2026-06-16 14:04:05 +0300

    flask

--- myduka/database.py
+++ myduka/database.py
@@ -1,120 +1,109 @@
-# import psycopg2
-# #establishing a connection to a postgress db
-# conn = psycopg2.connect(host='localhost', port=5432, user='postgres', password='2201', dbname='myduka')
-# #cur object
-# cur = conn.cursor()
-
-# cur.execute("select * from products")
-# products_data = cur.fetchall()
-# print(products_data)
-
-# #insert two records in sqlshell use psycopg2 to display them in your terminal
-
-# # import psycopg2
-
-# # # establishing a connection to a postgres db
-# # conn = psycopg2.connect(host='localhost', port=5432, user='postgres', password='2201', dbname='myduka')
-
-# # # cur object
-# # cur = conn.cursor()
-
-# # # 1. Insert a sale (assuming milk has an id of 1)
-# # # We pass pid=1, quantity=5, and product_name='milk'
-# # cur.execute("INSERT INTO sales (pid, quantity, product_name) VALUES (%s, %s, %s)", (1, 5, 'milk'))
-
-# # # 2. IMPORTANT: Commit the changes to save them permanently
-# # conn.commit()
-
-# # print("Sale inserted successfully!")
-
-# # 3. Read it back to verify
-# cur.execute("SELECT * FROM sales")
-# sales_data = cur.fetchall()
-# print(sales_data)
-
-# # # close connection
-# # cur.close()
-# # conn.close()
-
 import psycopg2
-#establishing a connection to a postgres db
-conn=psycopg2.connect(host='localhost',port=5432,user='postgres',password='2201',dbname='myduka')
- #cursor object
-cur=conn.cursor()
-def get_products():
-    cur.execute('select * from products')
-    products_data=cur.fetchall()
-    return products_data
 
-# cur.execute("insert into products(name,buying_price,selling_price)values('wallet',300,500)")
-# conn.commit()
-# print(products_data)
+#establishing a connection to a postgres db
+conn = psycopg2.connect(host='localhost',port=5432,user='postgres',password='2201',dbname='myduka')
+
+#cur object
+cur = conn.cursor()
+
+def get_products():
+    cur.execute("select * from products")
+    products_data = cur.fetchall()
+    return products_data
+    
 
 def get_sales():
-    cur.execute('select * from sales')
-    sales_data=cur.fetchall()
+    cur.execute("select * from sales")
+    sales_data = cur.fetchall()
     return sales_data
 
-def insert_products2(values):
-    cur.execute(f"insert into products(name,buying_price,selling_price)values{values}")
-    conn.commit()
-
-product1=('tissue',35,50)
-product2=('valon',120,150)
-
-insert_products2(product1)
-insert_products2(product2)
-products_data=insert_products2
-print(products_data)
-
-def insert_products2(values):
-    cur.execute(f"insert into products(name,buying_price,selling_price)values(%s,%s,%s)",values)
-    conn.commit()
-
-product3=('charger',200,350)
-# insert_products2(product3)
-products_data=insert_products2
-print(products_data)
-# Q1
-def get_stocks():
+def get_stock():
     cur.execute("select * from stock")
-    stocks_data = cur.fetchall()
-    return stocks_data
+    stock_data = cur.fetchall()
+    return stock_data
 
-stocks_data = get_stocks()
-print(stocks_data)
 
-#insert sales() 
+def get_data(table):
+    cur.execute(f"select * from {table}")
+    data = cur.fetchall()
+    return data
+
+
+def insert_products(values):
+    cur.execute(f"insert into products(name,buying_price, selling_price)values{values}")
+    conn.commit()
+
+product1 = ('shoes',2000,2500)
+product2 = ('phone',10000,15000)
+
+insert_products(product1)
+insert_products(product2)
+
+
+def insert_products2(values):
+    cur.execute("insert into products(name,buying_price,selling_price)values(%s,%s,%s)", values)
+    conn.commit()
+
 
 def insert_sales(values):
-    cur.execute("insert into sales(pid,quantity)values(%s,%s)",values)
+    cur.execute("insert into sales(pid,quantity)values(%s,%s)", values)
     conn.commit()
 
-sales1=(4,8)
-sales2=(3,12)
-sales3=(5,20)
 
-
-insert_sales(sales1)
-insert_sales(sales2)
-insert_sales(sales3)
-
-
-sales_data=get_sales()
-print(sales_data)
-
-# -> insert_stock()
 def insert_stock(values):
-    cur.execute("insert into stock(pid,stock_quantity)values(%s,%s)",values)
+    cur.execute("insert into stock(pid,stock_quantity)values(%s,%s)", values)
     conn.commit()
 
-stock1=(2,16)
-stock2=(5,21)
-stock3=(7,35)
 
-insert_stock(stock1)
-insert_stock(stock2)
-insert_stock(stock3)
+product3 = ('laptop',40000,50000)
+insert_products2(product3)
 
-stocks_data = get_stocks()
-print(stocks_data)
+
+def sales_per_day():
+    cur.execute("""
+      select date(sales.created_at) as date, sum(sales.quantity * products.selling_price) as
+      total_sales from sales join products on products.id = sales.pid  group by date;
+    """)
+    daily_sales = cur.fetchall()
+    return daily_sales
+
+
+def profit_per_day():
+    cur.execute("""
+        select date(sales.created_at) as date, sum(sales.quantity *( products.selling_price -
+        products.buying_price)) as total_sales from sales join products on products.id = sales.pid
+         group by date;
+    
+    """)
+    daily_profit = cur.fetchall()
+    return daily_profit
+
+
+
+def sales_per_product():
+    cur.execute("""
+        select products.name as p_name , sum(sales.quantity * products.selling_price)  as total_sales
+        from products join sales on sales.pid = products.id group by p_name;
+    """)
+    product_sales = cur.fetchall()
+    return product_sales
+
+
+def profit_per_product():
+    cur.execute("""
+        select products.name as p_name , sum(sales.quantity *( products.selling_price - 
+        products.buying_price))  as total_sales from products join sales on sales.pid = products.id group by p_name;
+    """)
+    product_profit = cur.fetchall()
+    return product_profit
+
+
+def check_available_stock(pid):
+    cur.execute("select sum(stock.stock_quantity) from stock where pid = %s",(pid,))
+    total_stock = cur.fetchone()[0] or 0
+
+    cur.execute("select sum(sales.quantity) from sales where pid = %s",(pid,))
+    total_sold = cur.fetchone()[0] or 0
+
+    return total_stock - total_sold
+
--- myduka/main.py
+++ myduka/main.py
@@ -1,50 +1,89 @@
-from flask import Flask,  render_template, request, redirect, url_for
-from database import get_products, get_sales, get_stocks, insert_products2
-# Create a Flask application instance
+from flask import Flask, render_template,request,redirect,url_for
+from database import get_products,get_sales,get_stock,insert_products,check_available_stock,insert_sales
+
+#Flask Instance
 app = Flask(__name__)
 
+#index route
 @app.route('/')
 def home():
-    number = 1000
-    return render_template('index.html', x=number)
+    return render_template("index.html")
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/products')
+#products route
+@app.route("/products")
 def products():
-    products_data = get_products()  # Fetch products from the database
-    return render_template('products.html', products_data = products_data)  # Pass products data to the template
+    products_data = get_products()
+    return render_template('products.html',products_data=products_data)
 
-@app.route('/add_products', methods=['GET', 'POST'])
+
+@app.route('/add_products',methods=['GET','POST'])
 def add_products():
     if request.method == 'POST':
         product_name = request.form['p_name']
         buying_price = request.form['b_price']
         selling_price = request.form['s_price']
 
-        new_product = (product_name, buying_price,selling_price)
-        insert_products2(new_product)
-        print("product added successfully")
-    
-    return redirect (url_for('products'))
+        new_product = (product_name,buying_price,selling_price)
+        insert_products(new_product)
+        print("product added successfully") 
+
+    return redirect(url_for('products'))
+
+
+
+#sales route
+@app.route('/sales')
+def sales():
+    sales_data = get_sales()
+    products = get_products()
+    return render_template('sales.html',sales_data=sales_data,products=products)
+
+
+
+@app.route('/make_sale',methods=['GET','POST'])
+def make_sale():
+    if request.method == 'POST':
+        pid = request.form['pid']
+        quantity = request.form['quantity']
+
+        new_sale = (pid,quantity)
+        available_stock = check_available_stock(pid)
+
+        if available_stock < float(quantity):
+            print("Insufficient stock,add more")
+            return redirect(url_for('sales'))
+
+        insert_sales(new_sale)
+        print("Sale added successfully")
+    return redirect(url_for('sales'))
+
+
+
+
+#stock route
+@app.route('/stock')
+def stock():
+    stock_data = get_stock()
+    products = get_products()
+    return render_template('stock.html',stock_data=stock_data,products=products)
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
-@app.route('/sales')
-def sales():
-    sales_data = get_sales()
-    return render_template('sales.html', sales_data = sales_data)
 
-@app.route('/stock')
-def stock():
-    return render_template('stock.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-app.run()
+
+
+#run your application
+app.run(debug=True)
